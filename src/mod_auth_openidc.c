08d3db175daf6359c36f5e94d304e54e35fd29cb
Hans Zandbelt <hzandbelt@pingidentity.com>
2015-09-20 09:08:00 +0200

    support (non-sid-based) HTTP logout spec
    http://openid.net/specs/openid-connect-logout-1_0.html

--- src/mod_auth_openidc.c
+++ src/mod_auth_openidc.c
@@ -1724,10 +1724,29 @@ static int oidc_handle_logout_request(request_rec *r, oidc_cfg *c,
 		oidc_session_kill(r, session);
 	}
 
-	/* see if this is the OP calling us, in which case we return HTTP 200 and a transparent pixel */
-	if (oidc_is_get_style_logout(url))
-		return oidc_util_http_send(r, (const char *) &oidc_transparent_pixel,
-				sizeof(oidc_transparent_pixel), "image/png", DONE);
+	/* see if this is the OP calling us */
+	if (oidc_is_get_style_logout(url)) {
+
+		/* set recommended cache control headers */
+		apr_table_add(r->err_headers_out, "Cache-Control",
+				"no-cache, no-store");
+		apr_table_add(r->err_headers_out, "Pragma", "no-cache");
+		apr_table_add(r->err_headers_out, "P3P", "CAO PSA OUR");
+		apr_table_add(r->err_headers_out, "Expires", "0");
+		apr_table_add(r->err_headers_out, "X-Frame-Options", "DENY");
+
+		/* see if this is PF-PA style logout in which case we return a transparent pixel */
+		const char *accept = apr_table_get(r->headers_in, "Accept");
+		if ((accept) && strstr(accept, "image/png")) {
+			return oidc_util_http_send(r,
+					(const char *) &oidc_transparent_pixel,
+					sizeof(oidc_transparent_pixel), "image/png", DONE);
+		}
+
+		/* standard HTTP based logout: should be called in an iframe from the OP */
+		return oidc_util_html_send(r, "Logged Out", NULL, NULL,
+				"<p>Logged Out</p>", DONE);
+	}
 
 	/* see if we don't need to go somewhere special after killing the session locally */
 	if (url == NULL)
