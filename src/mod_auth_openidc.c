a36b4cbea94b8be99a35eb102cc65ccc6498690f
Hans Zandbelt <hans.zandbelt@zmartzone.eu>
2017-03-14 10:34:58 +0100

    fix reference to old oidc_authz_worker22 naming
    Signed-off-by: Hans Zandbelt <hans.zandbelt@zmartzone.eu>

--- src/mod_auth_openidc.c
+++ src/mod_auth_openidc.c
@@ -3314,7 +3314,7 @@ int oidc_auth_checker(request_rec *r) {
 		oidc_util_json_merge(r, id_token, claims);
 
 	/* dispatch to the <2.4 specific authz routine */
-	int rc = oidc_authz_worker(r, claims ? claims : id_token, reqs,
+	int rc = oidc_authz_worker22(r, claims ? claims : id_token, reqs,
 			reqs_arr->nelts);
 
 	/* cleanup */
