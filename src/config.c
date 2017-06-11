97c20688dd5a9cdc450edecae85ea021503ff0da
Hans Zandbelt <hans.zandbelt@zmartzone.eu>
2017-06-11 14:13:33 +0200

    add cjose version to startup message
    Signed-off-by: Hans Zandbelt <hans.zandbelt@zmartzone.eu>

--- src/config.c
+++ src/config.c
@@ -1894,18 +1894,18 @@ static int oidc_post_config(apr_pool_t *pool, apr_pool_t *p1, apr_pool_t *p2,
 	}
 
 	ap_log_error(APLOG_MARK, APLOG_INFO, 0, s,
-			"%s - init - %s, Elliptic Curve (EC)=%s, Galois Counter Mode (GCM)=%s, Redis=%s",
+			"%s - init - cjose %s, %s, EC=%s, GCM=%s, Redis=%s",
 			NAMEVERSION,
+			cjose_version(),
 			OPENSSL_VERSION_TEXT,
-			OIDC_JOSE_EC_SUPPORT  ? "yes" : "no",
+			OIDC_JOSE_EC_SUPPORT ? "yes" : "no",
 			OIDC_JOSE_GCM_SUPPORT ? "yes" : "no",
 #ifdef USE_LIBHIREDIS
 			"yes"
 #else
 			"no"
 #endif
-		);
-
+			);
 
 	curl_global_init(CURL_GLOBAL_ALL);
 	OpenSSL_add_all_digests();
