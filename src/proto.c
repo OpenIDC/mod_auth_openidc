c075e7cdaf1b483feab800a1cf7e3896f98b0efc
Hans Zandbelt <hans.zandbelt@zmartzone.eu>
2018-05-19 10:25:41 +0200

    explicitly set kid in encrypted request object
    - ensures compatibility with cjose >= 0.6.0
    - bump to 2.3.6rc0
    
    Signed-off-by: Hans Zandbelt <hans.zandbelt@zmartzone.eu>

--- src/proto.c
+++ src/proto.c
@@ -376,6 +376,9 @@ char *oidc_proto_create_request_object(request_rec *r,
 		if (jwe->header.enc == NULL)
 			jwe->header.enc = apr_pstrdup(r->pool, CJOSE_HDR_ENC_A128CBC_HS256);
 
+		if (jwk->kid != NULL)
+			jwe->header.kid = jwk->kid;
+
 		if (oidc_jwt_encrypt(r->pool, jwe, jwk, cser,
 				&serialized_request_object, &err) == FALSE) {
 			oidc_error(r, "encrypting JWT failed: %s",
