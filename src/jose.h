11c280c4cbe0e0e8709e6dfc736b6ea6218cc718
Hans Zandbelt <hans.zandbelt@zmartzone.eu>
2018-12-31 11:05:54 +0100

    release 2.3.10
    Signed-off-by: Hans Zandbelt <hans.zandbelt@zmartzone.eu>

--- src/jose.h
+++ src/jose.h
@@ -18,7 +18,7 @@
  */
 
 /***************************************************************************
- * Copyright (C) 2017-2018 ZmartZone IAM
+ * Copyright (C) 2017-2019 ZmartZone IAM
  * Copyright (C) 2013-2017 Ping Identity Corporation
  * All rights reserved.
  *
