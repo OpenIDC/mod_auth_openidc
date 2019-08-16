c0382f6c3177ee47c297bfeb9ba55c0d4882be32
Hans Zandbelt <hans.zandbelt@zmartzone.eu>
2019-08-16 22:28:04 +0200

    revert 3d95b4a3fbc493c6acc745626ac33143eb4968bf
    don't return early from the content handler
    
    Signed-off-by: Hans Zandbelt <hans.zandbelt@zmartzone.eu>

--- src/mod_auth_openidc.c
+++ src/mod_auth_openidc.c
@@ -4127,7 +4127,7 @@ int oidc_content_handler(request_rec *r) {
 	oidc_cfg *c = ap_get_module_config(r->server->module_config,
 			&auth_openidc_module);
 	return oidc_util_request_matches_url(r, oidc_get_redirect_uri(r, c)) ?
-			DONE : DECLINED;
+			OK : DECLINED;
 }
 
 extern const command_rec oidc_config_cmds[];
