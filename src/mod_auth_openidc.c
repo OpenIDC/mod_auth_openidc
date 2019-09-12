1974f6a3e3030c38d31ecaa722736bf4562c1187
Hans Zandbelt <hans.zandbelt@zmartzone.eu>
2019-09-12 22:07:39 +0200

    use liboauth 1.2.0 with new request header API
    Signed-off-by: Hans Zandbelt <hans.zandbelt@zmartzone.eu>

--- src/mod_auth_openidc.c
+++ src/mod_auth_openidc.c
@@ -50,36 +50,40 @@
 OAUTH2_APACHE_LOG(auth_openidc)
 
 typedef struct openidc_cfg_dir_t {
-  oauth2_cfg_openidc_t *openidc;
-  oauth2_cfg_target_pass_t *target_pass;
+	oauth2_cfg_openidc_t *openidc;
+	oauth2_cfg_target_pass_t *target_pass;
 } openidc_cfg_dir_t;
 
-static apr_status_t openidc_cfg_dir_cleanup(void *data) {
-  openidc_cfg_dir_t *cfg = (openidc_cfg_dir_t *)data;
-  oauth2_cfg_openidc_free(NULL, cfg->openidc);
-  oauth2_cfg_target_pass_free(NULL, cfg->target_pass);
-  oauth2_mem_free(cfg);
-  return APR_SUCCESS;
+static apr_status_t openidc_cfg_dir_cleanup(void *data)
+{
+	openidc_cfg_dir_t *cfg = (openidc_cfg_dir_t *)data;
+	oauth2_cfg_openidc_free(NULL, cfg->openidc);
+	oauth2_cfg_target_pass_free(NULL, cfg->target_pass);
+	oauth2_mem_free(cfg);
+	return APR_SUCCESS;
 }
 
-static void *openidc_cfg_dir_create(apr_pool_t *pool, char *path) {
-  openidc_cfg_dir_t *cfg = oauth2_mem_alloc(sizeof(openidc_cfg_dir_t));
-  oauth2_cfg_openidc_free(NULL, cfg->openidc);
-  cfg->openidc = oauth2_cfg_openidc_init(NULL);
-  cfg->target_pass = oauth2_cfg_target_pass_init(NULL);
-  apr_pool_cleanup_register(pool, cfg, openidc_cfg_dir_cleanup,
-                            openidc_cfg_dir_cleanup);
-  return cfg;
+static void *openidc_cfg_dir_create(apr_pool_t *pool, char *path)
+{
+	openidc_cfg_dir_t *cfg = oauth2_mem_alloc(sizeof(openidc_cfg_dir_t));
+	oauth2_cfg_openidc_free(NULL, cfg->openidc);
+	cfg->openidc = oauth2_cfg_openidc_init(NULL);
+	cfg->target_pass = oauth2_cfg_target_pass_init(NULL);
+	apr_pool_cleanup_register(pool, cfg, openidc_cfg_dir_cleanup,
+				  openidc_cfg_dir_cleanup);
+	return cfg;
 }
 
-static void *openidc_cfg_dir_merge(apr_pool_t *pool, void *b, void *a) {
-  openidc_cfg_dir_t *cfg = openidc_cfg_dir_create(pool, NULL);
-  openidc_cfg_dir_t *base = b;
-  openidc_cfg_dir_t *add = a;
-  oauth2_cfg_openidc_merge(NULL, cfg->openidc, base->openidc, add->openidc);
-  oauth2_cfg_target_pass_merge(NULL, cfg->target_pass, base->target_pass,
-                               add->target_pass);
-  return cfg;
+static void *openidc_cfg_dir_merge(apr_pool_t *pool, void *b, void *a)
+{
+	openidc_cfg_dir_t *cfg = openidc_cfg_dir_create(pool, NULL);
+	openidc_cfg_dir_t *base = b;
+	openidc_cfg_dir_t *add = a;
+	oauth2_cfg_openidc_merge(NULL, cfg->openidc, base->openidc,
+				 add->openidc);
+	oauth2_cfg_target_pass_merge(NULL, cfg->target_pass, base->target_pass,
+				     add->target_pass);
+	return cfg;
 }
 
 // clang-format off
@@ -90,9 +94,12 @@ static const command_rec OAUTH2_APACHE_COMMANDS(auth_openidc)[] = {
 	{ NULL }
 };
 
+// clang-format on
+
 static int openidc_request_handler(oauth2_cfg_openidc_t *cfg,
-				  oauth2_cfg_target_pass_t *target_pass,
-				  oauth2_apache_request_ctx_t *ctx) {
+				   oauth2_cfg_target_pass_t *target_pass,
+				   oauth2_apache_request_ctx_t *ctx)
+{
 	int rv = DECLINED;
 	bool rc = false;
 	oauth2_http_response_t *response = NULL;
@@ -110,13 +117,17 @@ static int openidc_request_handler(oauth2_cfg_openidc_t *cfg,
 	rv = oauth2_http_response_status_code_get(ctx->log, response);
 	// TODO: HTTP_OK = 200?
 
-	// TODO: use Apache function to copy response headers/status
-	if (rv == HTTP_MOVED_TEMPORARILY) {
-		oauth2_apache_hdr_out_add(ctx->log, ctx->r,
-				OAUTH2_HTTP_HDR_LOCATION, oauth2_http_response_header_get(ctx->log, response, OAUTH2_HTTP_HDR_LOCATION));
+	if (oauth2_apache_http_response_set(ctx->log, response, ctx->r) ==
+	    false) {
+		rv = HTTP_INTERNAL_SERVER_ERROR;
+		goto end;
 	}
 
-	//oauth2_apache_target_pass(ctx, target_pass, source_token, json_token);
+	//	if (oauth2_apache_http_response_status_code_get() == 200)
+	//		rv = OK;
+
+	// oauth2_apache_target_pass(ctx, target_pass, source_token,
+	// json_token);
 
 end:
 
@@ -128,10 +139,10 @@ end:
 
 // TODO:
 bool _openidc_provider_resolver(oauth2_log_t *log,
-				    const oauth2_http_request_t *request,
-				    oauth2_openidc_provider_t **provider)
+				const oauth2_http_request_t *request,
+				oauth2_openidc_provider_t **provider)
 {
-    //json = json_load_file("/path/to/file.json", 0, &error);
+	// json = json_load_file("/path/to/file.json", 0, &error);
 
 	// TODO: free/global
 	*provider = oauth2_openidc_provider_init(log);
@@ -176,32 +187,37 @@ static int openidc_check_user_id_handler(request_rec *r)
 	ctx = OAUTH2_APACHE_REQUEST_CTX(r, auth_openidc);
 
 	// TODO:
-	oauth2_cfg_openidc_provider_resolver_set(ctx->log, cfg->openidc, _openidc_provider_resolver);
-	oauth2_cfg_openidc_passphrase_set(ctx->log, cfg->openidc, "password1234");
+	oauth2_cfg_openidc_provider_resolver_set(ctx->log, cfg->openidc,
+						 _openidc_provider_resolver);
+	oauth2_cfg_openidc_passphrase_set(ctx->log, cfg->openidc,
+					  "password1234");
 
 	oauth2_debug(ctx->log,
 		     "incoming request: \"%s?%s\" ap_is_initial_req=%d",
 		     r->parsed_uri.path, r->args, ap_is_initial_req(r));
 
 	if (strcasecmp((const char *)ap_auth_type(r), OPENIDC_AUTH_TYPE) == 0)
-		return openidc_request_handler(cfg->openidc, cfg->target_pass, ctx);
+		return openidc_request_handler(cfg->openidc, cfg->target_pass,
+					       ctx);
 
 	if (strcasecmp((const char *)ap_auth_type(r),
 		       OPENIDC_AUTH_TYPE_OPENIDC) == 0)
-		return openidc_request_handler(cfg->openidc, cfg->target_pass, ctx);
+		return openidc_request_handler(cfg->openidc, cfg->target_pass,
+					       ctx);
 
 	return DECLINED;
 }
 
 static void auth_openidc_register_hooks(apr_pool_t *p)
 {
-	static const char *const aszPre[] = { "mod_oauth2.c", NULL};
-	ap_hook_post_config(OAUTH2_APACHE_POST_CONFIG(auth_openidc), aszPre, NULL,
-			    APR_HOOK_MIDDLE);
+	static const char *const aszPre[] = {"mod_oauth2.c", NULL};
+	ap_hook_post_config(OAUTH2_APACHE_POST_CONFIG(auth_openidc), aszPre,
+			    NULL, APR_HOOK_MIDDLE);
 	ap_hook_check_authn(openidc_check_user_id_handler, aszPre, NULL,
 			    APR_HOOK_MIDDLE, AP_AUTH_INTERNAL_PER_CONF);
 }
 
+// clang-format off
 OAUTH2_APACHE_MODULE_DECLARE_EX(
 	auth_openidc,
 	openidc_cfg_dir_create,
