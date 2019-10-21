a1b2c26047aec50c059b881840c7e826fb5639cd
robert.kreger@dematic.com <0105May!2275>
2019-10-21 14:59:49 -0400

    Added scripts to automate the build process.

---
+++ AddSubModules.cmd
@@ -0,0 +1,3 @@
+git submodule add https://github.com/cisco/cjose cjose
+
+git submodule add https://github.com/microsoft/vcpkg.git vcpkg
--- bootstrap-windows.cmd
+++ bootstrap-windows.cmd
@@ -1,26 +1,28 @@
 @echo Requires git installed and an account on github
 
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
-git clone https://github.com/microsoft/vcpkg.git
 cd vcpkg
 call bootstrap-vcpkg.bat
 
-REM these are not needed because they come with Apache
-rem .\vcpkg install apr:x64-windows
-rem .\vcpkg install apr:x86-windows
+.\vcpkg install pcre:x64-windows
+.\vcpkg install pcre:x86-windows
+.\vcpkg install apr:x64-windows
+.\vcpkg install apr:x86-windows
 .\vcpkg install curl:x64-windows
 .\vcpkg install curl:x86-windows
 .\vcpkg install openssl:x64-windows
 .\vcpkg install openssl:x86-windows
 .\vcpkg install jansson:x64-windows
 .\vcpkg install jansson:x86-windows
+.\vcpkg integrate install
 
+cd..
 
 @echo Over changes to cjose and mod_auth_openidc so they compile on windows
-xcopy changes\*.* /r
+xcopy changes\*.* /r /q /y /s
 
 @echo Downloading Apache http x32 and x64 zip files.
-
+powershell .\download.ps1
 
 
 rem call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars32.bat"
