5b2b620dc47db3f20fd1dcccaf81bf6c5135b3de
Hans Zandbelt <hans.zandbelt@openidc.com>
2023-04-04 08:35:25 +0200

    also change sizeof(cjose_jwk_ec_curve) to sizeof(apr_uint32_t)
    Signed-off-by: Hans Zandbelt <hans.zandbelt@openidc.com>

--- src/jose.c
+++ src/jose.c
@@ -1500,7 +1500,7 @@ static apr_byte_t _oidc_jwk_ec_key_to_jwk(apr_pool_t *pool, EVP_PKEY *pkey,
 	apr_uint32_t b = htonl(crv);
 	*fp_len = sizeof(b) + ec_keyspec.xlen + ec_keyspec.ylen;
 	*fp = apr_pcalloc(pool, *fp_len);
-	memcpy(*fp, &b, sizeof(cjose_jwk_ec_curve));
+	memcpy(*fp, &b, sizeof(b));
 	memcpy(*fp + sizeof(b), ec_keyspec.x, ec_keyspec.xlen);
 	memcpy(*fp + sizeof(b) + ec_keyspec.xlen, ec_keyspec.y,
 			ec_keyspec.ylen);
