83d5d3abd5cc502a3847520466187ffddfe5da19
Ryan Flood <ryan.flood@dematic.com>
2024-11-13 12:19:02 -0600

    2.4.16.5 Update

--- build.cmd
+++ build.cmd
@@ -9,7 +9,7 @@ mkdir target\mod_auth_openidc\x64
 mkdir target\mod_auth_openidc\x64\Release
 copy x64\Release\mod_auth_openidc.dll target\mod_auth_openidc\x64\Release\mod_auth_openidc.dll
 
-del target\mod_auth_openidc-2.4.15.7-win64-VS16.zip
+del target\mod_auth_openidc-2.4.16.5-win64-VS16.zip
 
 REM This next step is perplexing.  Something verifies the case of the dll that it is loading and libeay32.dll must be uppercase 
 REM following the import statements. vcpkg creates a lower case dll, but the imports from the lib file have the dll name unppercase.
@@ -17,5 +17,5 @@ mkdir target\x64
 del target\x64\libcrypto-3-x64.dll
 copy  vcpkg\installed\x64-windows\bin\libcrypto-3-x64.dll target\x64\libcrypto-3-x64.dll
 
-powershell Compress-Archive -Path target\x64\libcrypto-3-x64.dll -Update -DestinationPath target\mod_auth_openidc-2.4.15.7-win64-VS16.zip
-powershell Compress-Archive -Path target\mod_auth_openidc\x64\Release\mod_auth_openidc.dll -Update -DestinationPath target\mod_auth_openidc-2.4.15.7-win64-VS16.zip
+powershell Compress-Archive -Path target\x64\libcrypto-3-x64.dll -Update -DestinationPath target\mod_auth_openidc-2.4.16.5-win64-VS16.zip
+powershell Compress-Archive -Path target\mod_auth_openidc\x64\Release\mod_auth_openidc.dll -Update -DestinationPath target\mod_auth_openidc-2.4.16.5-win64-VS16.zip
--- src/metadata.c
+++ src/metadata.c
@@ -53,6 +53,8 @@
 #include "proto/proto.h"
 #include "util.h"
 
+#define TOSTRING(x) #x
+
 #define OIDC_METADATA_SUFFIX_PROVIDER "provider"
 #define OIDC_METADATA_SUFFIX_CLIENT "client"
 #define OIDC_METADATA_SUFFIX_CONF "conf"
